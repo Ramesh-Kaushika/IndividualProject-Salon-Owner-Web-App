fd39e5c8e19d2622626e9eedb63b8543f7cb5b73
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-08-16 01:39:57 +0530

    Creating Routing part
    -2024-08-15-

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Navigate, Route, Routes} from "react-router-dom";
+import Register from "./pages/Register/Register.jsx";
+import Login from "./pages/Login/Login.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-   <h1>Hello World</h1>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <Routes>
+                    <Route path={'*'} element={<Navigate to={'/login'}/>}/>
+                    <Route path={'/register'} element={<Register/>}/>
+                    <Route path={'/login'} element={<Login/>}/>
+                </Routes>
+
+            </div>
+        </>
+    )
 }
 
 export default App
