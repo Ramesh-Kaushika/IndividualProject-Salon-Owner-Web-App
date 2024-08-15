ac61f4e7596238bf0486743bb76cf68a9daf0f5a
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-08-16 03:40:57 +0530

    Create route UI
    -2024-08-16-

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import './App.css'
 import {Navigate, Route, Routes} from "react-router-dom";
 import Register from "./pages/Register/Register.jsx";
 import Login from "./pages/Login/Login.jsx";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword.jsx";
+import Verify from "./pages/Verify/Verify.jsx";
+import CreateNewPassword from "./pages/CreateNewPassword/CreateNewPassword.jsx";
 
 function App() {
     const [count, setCount] = useState(0)
@@ -16,6 +19,10 @@ function App() {
                     <Route path={'*'} element={<Navigate to={'/login'}/>}/>
                     <Route path={'/register'} element={<Register/>}/>
                     <Route path={'/login'} element={<Login/>}/>
+                    <Route path={'/forgot-password'} element={<ForgotPassword/>}/>
+                    <Route path={'/verify'} element={<Verify/>}/>
+                    <Route path={'/create-new-password'} element={<CreateNewPassword/>}/>
+
                 </Routes>
 
             </div>
