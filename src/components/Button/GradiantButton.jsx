598c08708f1d84aba340ab8de03d3110031c3b54
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-30 14:11:13 +0530

    dashboard done
    -2024-10-30-

--- src/components/Button/GradiantButton.jsx
+++ src/components/Button/GradiantButton.jsx
@@ -1,18 +1,25 @@
 import {Button} from "@mui/material";
 import React from "react";
 import Dashboard from "../../pages/Dashboard/Dashboard.jsx";
+import DashboardSharpIcon from "@mui/icons-material/DashboardSharp";
 
-const GradiantButton = ({title}) => {
+const GradiantButton = ({title,startIcon,onClick}) => {
 
     return (
 
             <Button
+                onClick={onClick}
+                startIcon={startIcon}
                 sx={{
-
+                    display:'flex',
+                    flexDirection:'column',
+                    justifyContent:'center',
+                    alignItems:'center',
+                    boxShadow: '0 4px 10px rgba(0, 0, 0, 1.3)', // Add shadow here
                     fontWeight:'bold',
                     fontSize:16,
                     border:10,
-                    borderColor:'#009688',
+                    borderColor:' #009688',
                     borderRadius:10,
                     margin:1,
                     width: {xs: '65%', sm: '45%', md: '35%', lg: '25%'}, // Responsive widths
@@ -21,6 +28,7 @@ const GradiantButton = ({title}) => {
                     color: 'white',
                     '&:hover': {
                         background: 'linear-gradient(45deg, #009688 30%, #001a17 90%)',
+                        boxShadow: '0 4px 20px rgba(0, 0, 0, 0.3)', // Add shadow here
                     },
                 }}
             >
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import {Button} from "@mui/material";
 import GradiantButton from "../../components/Button/GradiantButton.jsx";
+import GroupIcon from "@mui/icons-material/Group";
+import BadgeIcon from "@mui/icons-material/Badge";
+import LocalLibrarySharpIcon from "@mui/icons-material/LocalLibrarySharp";
+import NotificationAddSharpIcon from "@mui/icons-material/NotificationAddSharp";
+import AttachMoneySharpIcon from "@mui/icons-material/AttachMoneySharp";
+import AddIcCallSharpIcon from "@mui/icons-material/AddIcCallSharp";
+import ThumbUpOffAltSharpIcon from "@mui/icons-material/ThumbUpOffAltSharp";
+
 const Dashboard = () => {
 
     return (
-        <div style={{display:'flex', justifyContent:'center',alignItems:'center',flexWrap:'wrap'}}>
-       <GradiantButton title={'Client Base'}/>
-       <GradiantButton title={'Employees'}/>
-       <GradiantButton title={'Appointments'}/>
-       <GradiantButton title={'Notifications'}/>
-       <GradiantButton title={'Promotions'}/>
-       <GradiantButton title={'Contacts'}/>
-       <GradiantButton title={'Feedbacks'}/>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap'}}>
+            <GradiantButton title={'Client Base'}
+                            startIcon={<GroupIcon style={{fontSize: '80px',  }}/>}
+            />
+            <GradiantButton title={'Employees'}
+                            startIcon={<BadgeIcon style={{fontSize: '80px',  }}/>}
+            />
+            <GradiantButton title={'Appointments'}
+                            startIcon={<LocalLibrarySharpIcon style={{fontSize: '80px',  }}/>}
+            />
+            <GradiantButton title={'Notifications'}
+                            startIcon={<NotificationAddSharpIcon style={{fontSize: '80px',  }}/>}
+            />
+            <GradiantButton title={'Promotions'}
+                            startIcon={<AttachMoneySharpIcon style={{fontSize: '80px',  }}/>}
+            />
+            <GradiantButton title={'Contacts'}
+                            startIcon={<AddIcCallSharpIcon style={{fontSize: '80px',  }}/>}
+            />
+            <GradiantButton title={'Feedbacks'}
+                            startIcon={<ThumbUpOffAltSharpIcon style={{fontSize: '80px',  }}/>}
+            />
         </div>
     );
 }
