d4d6d9e3dba23fd306c6831f117c33b2c849412c
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-11-01 03:26:49 +0530

    register done
    -2024-11-01-

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Box,
     Button,
@@ -12,8 +12,13 @@ import {
 } from "@mui/material";
 import {Visibility, VisibilityOff} from "@mui/icons-material";
 import {Link} from "react-router-dom";
+import instance from "../../services/Axios/AxiosOrder.jsx";
 
 const Register = () => {
+
+    const [regName, setRegName] = useState('')
+    const [regPassword, setRegPassword] = useState('')
+
     const [showPassword, setShowPassword] = React.useState(false);
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -21,6 +26,21 @@ const Register = () => {
     const handleMouseDownPassword = (event) => {
         event.preventDefault();
     };
+
+    function loginHandleClick() {
+        instance.post('/api/v1/admin/register', {
+            adminName: regName,
+            password: regPassword
+        })
+            .then(function (response) {
+                // console.log(response.data.token);
+                // // localStorage.setItem('stToken',response.data.token)
+                // window.location.reload();
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
     return (
         <Box
@@ -56,26 +76,13 @@ const Register = () => {
                     sx={{width: '35ch'}}
                     id="standard-basic"
                     label="Name"
-                    variant="standard"/>
-                <TextField
-                    sx={{width: '35ch'}}
-                    id="standard-basic"
-                    label="E-mail"
-                    variant="standard"/>
-                <TextField
-                    sx={{width: '35ch'}}
-                    id="standard-basic"
-                    label="E-mail"
-                    variant="standard"/>
-                <TextField
-                    sx={{width: '35ch'}}
-                    id="standard-basic"
-                    label="E-mail"
-                    variant="standard"/>
-
+                    variant="standard"
+                    onChange={(e) => setRegName(e.target.value)}
+                />
                 <FormControl sx={{width: '35ch'}} variant="standard">
                     <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
                     <Input
+                        onChange={(e) => setRegPassword(e.target.value)}
                         id="standard-adornment-password"
                         type={showPassword ? 'text' : 'password'}
                         endAdornment={
@@ -94,7 +101,7 @@ const Register = () => {
 
                 <Divider sx={{ width: '100%', marginY: 2 }} />
                 <Link to={'/login'}>
-                    <Button sx={{backgroundColor: '#212121', width: '35ch', textTransform: 'none', }} variant="contained">Sign Up</Button>
+                    <Button onClick={() => loginHandleClick()} sx={{backgroundColor: '#212121', width: '35ch', textTransform: 'none', }} variant="contained">Sign Up</Button>
                 </Link>
             </Box>
         </Box>
