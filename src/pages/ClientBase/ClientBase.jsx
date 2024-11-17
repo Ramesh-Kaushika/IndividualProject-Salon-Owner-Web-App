6ca550bcaa25dd9f732c8236c7e0dd4cfb589f5f
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-11-17 23:52:49 +0530

    update
    -2024-11-17-

--- src/pages/ClientBase/ClientBase.jsx
+++ src/pages/ClientBase/ClientBase.jsx
@@ -23,7 +23,7 @@ const ClientBase = () => {
     if (error) return <div>Error loading clients: {error.message}</div>;
 
 
-    // Function to handle deleting a client910111123456
+    // Function to handle deleting a client
     const deleteClient = (id) => {
         setClients(clients.filter((client) => client.id !== id));
     };
