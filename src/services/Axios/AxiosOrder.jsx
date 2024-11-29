4393781bf4d1f66730c6aeae42c27159546fc3e1
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-11-29 23:46:25 +0530

    update
    -2024-11-29-

--- src/services/Axios/AxiosOrder.jsx
+++ src/services/Axios/AxiosOrder.jsx
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'http://localhost:8080',
     timeout: 1000,
     // headers: {'X-Custom-Header': 'foobar' 1234567891234 }
-    // headers: {'X-Custom-Header': 'foobar' 123456789}
+    // headers: {'X-Custom-Header': 'foobar' 12345678}
 });
 
 export default instance;
